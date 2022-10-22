cdbcd787be8677f4182a572282e9ca22d67e29e6
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-10-22 13:53:15 -0500

    commit tree 969a11346fc26c0ea553ab12fd31fe8670dcb50f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+const events = "surprise"
+
+function writeCards(names, events) {
+    const writtenCards = []
+    for (let i=0; i < names.length; i++) {
+        writtenCards.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+
+    return writtenCards;
+}
+
+function countDown(number) {
+    let countDown = number;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
